f2fc757a7a14ff47aaa5f933e25c20463cf72d30
1234persons <gibreeno@outlook.com>
2024-09-04 08:22:11 -0400

    new file:   src/Chapter 8/MySavings/MySavingsClient.java 	new file:   src/Chapter 8/MySavings/MySavingsCode.java

---
+++ src/Chapter 8/MySavings/MySavingsClient.java
@@ -0,0 +1,14 @@
+public class MySavingsClient {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int userInput;
+
+        System.out.println("1. Show total in bank.");
+        System.out.println("2. Add a penny.");
+        System.out.println("3. Add a nickel.");
+        System.out.println("4. Add a penny.");
+        System.out.println("5. Add a penny.");
+        System.out.println("6. Add a penny.");
+    }
+}
