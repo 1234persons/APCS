360c31ada48f4548d42dd1a8263d4ddb2a8dcebf
1234persons <gibreeno@outlook.com>
2024-09-18 07:45:13 -0400

    new file:   src/Chapter 9/Circle/Circle.java 	new file:   src/Chapter 9/Circle/CircleMain.java 	new file:   src/Chapter 9/Circle/Disk.java 	new file:   src/Chapter 9/Circle/TestDisk.java

---
+++ src/Chapter 9/Circle/CircleMain.java
@@ -0,0 +1,38 @@
+package Circle;
+
+import java.text.NumberFormat;
+
+
+public class CircleMain {
+    public static void main(String[] args) {
+        
+        Circle spot = new Circle();
+        Circle spotP2 = new Circle(5, "Red");
+
+        NumberFormat number = NumberFormat.getNumberInstance();
+
+        if (spot.equals(spotP2)) {
+            System.out.println("Objects are equal");
+        } else {
+            System.out.println("Objects are not equal");
+        }
+
+        System.out.println(spot);
+        System.out.println(spotP2);
+  
+        spot.setRadius(10);
+
+        System.out.println("The radius is " + spot.getRadius());
+        System.out.println("The area is " + spot.area());
+        System.out.println("The circumference is " + number.format(spot.circumference()));
+        System.out.println("The color of the first circle is: " + spot.getColor() + "\n");
+
+        System.out.println("The radius of the second circle is: " + spotP2.getRadius());
+        System.out.println("The area is: " + spotP2.area());
+        System.out.println("The circumference is: " + number.format(spotP2.circumference()));
+        System.out.println("The color of the first circle is: " + spotP2.getColor() + "\n");
+
+        Circle.displayAreaFormula();
+
+    }
+}
---
+++ src/Chapter 9/Circle/Disk.java
@@ -0,0 +1,81 @@
+import Circle.Circle;
+
+public class Disk extends Circle {
+    private double thickness;
+
+    /*
+     * constructor
+     * pre: none
+     * post: creates disk object with radius r and double t
+     */
+
+    public Disk(double r, double t) {
+        super(r);
+        thickness = t;
+    }
+
+    /*
+     * Changes thickness of the disk
+     * pre: none
+     * post: thickness is changed
+     */
+
+    public void setThickness(double newThickness) {
+        thickness = newThickness;
+    }
+
+    /*
+     * Returns the thickness of the disk
+     * pre: none
+     * post: thickness is returned
+     */
+
+    public double getThickness() {
+        return (thickness);
+    }
+
+    /*
+     * Return the disk's volume
+     * pre: none
+     * post: the volume of the disk is returned
+     */
+
+    public double volume() {
+        double v = super.area() * thickness;
+        return(v);
+    }
+
+    /*
+     * Determines if the object is equal to another disk object
+     * pre: disk d is an object
+     * post: equality is determined
+     */
+
+     public boolean equals(Object d) {
+
+        Disk testObj = (Disk) d;
+
+        if (testObj.getRadius() == super.getRadius() && testObj.getThickness() == super.getRadius()) {
+            return (true);
+        } else {
+            return (false);
+        }
+
+        
+
+    }
+
+    /*
+     * Returns a string representing the string object
+     * pre: none
+     * post: a string representing the string object is returned
+     */
+
+    public String toString() {
+        String diskString;
+
+        diskString = "The disk has a radius " + super.getRadius() + " and a thickness of " + thickness + ".";
+
+        return (diskString);
+    }
+}
