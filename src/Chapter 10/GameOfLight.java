2cdb702b041b04602d7cdd53ca50cd37f9bc9cb6
1234persons <gibreeno@outlook.com>
2024-10-24 08:56:19 -0400

    modified:   src/Chapter 10/GameOfLight.java

--- src/Chapter 10/GameOfLight.java
+++ src/Chapter 10/GameOfLight.java
@@ -6,33 +6,105 @@ public class GameOfLight {
 
         int userWidth = 0;
         int userLength;
+        int row;
+        int column;
+        int moves = 0;
 
         do {
-        System.out.println("Enter how wide you would like the grid: (3-9)");
-        if (input.hasNextInt()) {
+            System.out.println("Enter how wide you would like the grid: (3-9)");
             userWidth = input.nextInt();
-        }    
         } while (userWidth < 3 || userWidth > 9);
 
         do {
-        System.out.println("Enter the length of the grid: (3-9)");
-        userLength = input.nextInt();
+            System.out.println("Enter the length of the grid: (3-9)");
+            userLength = input.nextInt();
         } while (userWidth < 3);
 
         String[][] grid = new String[userLength][userWidth];
 
         for (int i = 0; i < userLength; i++) {
             for (int j = 0; j < userWidth; j++) {
-                //System.out.println(i + "=" + j);
-                grid[i][j]  = "O";
-            } 
+
+                grid[i][j] = "O";
+            }
+        }
+        printBoard(grid, userWidth, userLength);
+
+        while (true) {
+
+            do {
+                System.out.println("Enter a row to click on: ");
+                row = input.nextInt();
+            } while (row < 0 || row > userLength);
+
+            do {
+                System.out.println("Enter a column to click on: ");
+                column = input.nextInt();
+            } while (column < 0 || column > userWidth);
+            
+            setCell(grid, row, column);
+
+            if (row > 0) {
+                setCell(grid, row - 1, column);
+            }
+
+            if (row < userLength - 1) {
+                setCell(grid, row + 1, column);
+            }
+
+            if (column > 0) {
+                setCell(grid, row, column - 1);
+            }
+
+            if (column < userWidth - 1) {
+                setCell(grid, row, column + 1);
+            }
+
+            printBoard(grid, userWidth, userLength);
+            if (gameOver(grid)) {
+                System.out.println("You win!");
+                System.out.println("It took you " + moves + " moves");
+                break;
+            } else {
+                moves += 1;
+            }
+        }
+    }
+
+    public static void setCell(String[][] grid, int row, int column) {
+        if (grid[row][column].equalsIgnoreCase("O")) {
+        grid[row][column] = "L";
+        } else {
+            grid[row][column] = "O";
+        }
+    }
+
+    public static void printBoard(String[][] board, int width, int length) {
+
+        System.out.print("  ");
+        for (int i = 0; i < width; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < length; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < width; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+
         }
 
-        for (int i = 0; i < userLength; i++) {
-            for (int j = 0; j < userWidth; j++) {
-                System.out.print(grid[i][j] + " ");
-            } 
-            System.out.println();
+    }
+
+    public static boolean gameOver(String[][] board) {
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j].equals("O"))
+                    return false;
+            }
         }
+        return true;
     }
 }
