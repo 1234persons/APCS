73b1980af4a29297821619913ca94306c2f2b133
1234persons <gibreeno@outlook.com>
2024-11-25 11:34:53 -0500

    modified:   src/Chapter 10/Life.java

--- src/Chapter 10/Life.java
+++ src/Chapter 10/Life.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Life {
@@ -5,16 +6,30 @@ public class Life {
         Scanner input = new Scanner(System.in);
 
         String[][] grid = new String[10][10];
+        String userInput;
+        ArrayList<Integer> rowCords = new ArrayList<Integer>();
+        ArrayList<Integer> colCords = new ArrayList<Integer>();
+
+        do {
+            System.out.println("Enter the coordinates you would like to place living cells.");
+            System.out.println("row");
+            rowCords.add(input.nextInt());
+
+            System.out.println("column");
+            colCords.add(input.nextInt());
+
+            System.out.println("Would you like to enter more coordinates? (y for yes)");
+            userInput = input.next();
+        } while (!userInput.equalsIgnoreCase("n"));
 
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (i > 4 && i < 6 && j > 3 && j < 7) {
-                    grid[i][j] = "X";
-                } else {
-                    grid[i][j] = "O";
-                }
-
+                grid[i][j] = "O";
             }
+        }
+
+        for (int i = 0; i < rowCords.size(); i++) {
+            grid[rowCords.get(i)][colCords.get(i)] = "X";
         }
         // printBoard(grid, 10, 10);
 
@@ -26,59 +41,50 @@ public class Life {
                 break;
             }
             setCells(grid);
-            input.nextInt();
+
+            System.out.println("Continue? (y for yes, n for no)");
+            if (input.next().equalsIgnoreCase("n")) {
+                break;
+            }
         }
     }
 
     public static void setCells(String[][] grid) {
         int neighborNum = 0;
 
-
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
 
-                // Check cell
-                if (grid[i][j].equals("X")) {
+                // check the cell above
+                if (i > 0 && grid[i - 1][j].equals("X")) {
                     neighborNum++;
                 }
-
-                // Check cell below and to the left
-                if (grid[i - 1][j - 1].equals("X") && i - 1 > 0 && i  < 10 && j - 1 > 0 && j < 10) {
+                // check the cell below
+                if (i < grid.length - 1 && grid[i + 1][j].equals("X")) {
                     neighborNum++;
                 }
-
-                // Check cell below
-                if (grid[i - 1][j].equals("X") && i - 1 > 0 && i  < 10 && j - 1 > 0 && j < 10) {
+                // check the cell to the left
+                if (j > 0 && grid[i][j - 1].equals("X")) {
                     neighborNum++;
                 }
-
-                // Check cell below and to the right
-                if (grid[i - 1][j + 1].equals("X") && i - 1 > 0 && i < 10 && j - 1 > 0 && j + 1 < 10) {
+                // check the cell to the right
+                if (j < grid[1].length - 1 && grid[i][j + 1].equals("X")) {
                     neighborNum++;
                 }
-
-                // Check cell to the right
-                if (grid[i][j + 1].equals("X") && i > 0 && i < 10 && j > 0 && j + 1 < 10) {
+                // check the cell above and to the left
+                if (i > 0 && j > 0 && grid[i - 1][j - 1].equals("X")) {
                     neighborNum++;
                 }
-
-                // Check cell to the left
-                if (grid[i][j - 1].equals("X") && i > 0 && i < 10 && j - 1 > 0 && j < 10) {
+                // check the cell above and to the right
+                if (i > 0 && j < grid[1].length - 1 && grid[i - 1][j + 1].equals("X")) {
                     neighborNum++;
                 }
-
-                // Check cell above and to the left
-                if (grid[i + 1][j - 1].equals("X") && i > 0 && i + 1 < 10 && j - 1 > 0 && j < 10) {
+                // check the cell below and to the left
+                if (i < grid.length - 1 && j > 0 && grid[i + 1][j - 1].equals("X")) {
                     neighborNum++;
                 }
-
-                // Check cell above
-                if (grid[i + 1][j].equals("X") && i > 0 && i + 1 < 10 && j > 0 && j < 10) {
-                    neighborNum++;
-                }
-
-                // Check cell above and to the right
-                if (grid[i + 1][j + 1].equals("X") && i > 0 && i + 1 < 10 && j > 0 && j + 1 < 10) {
+                // check the cell below and to the right
+                if (i < grid.length - 1 && j < grid[1].length - 1 && grid[i + 1][j + 1].equals("X")) {
                     neighborNum++;
                 }
 
@@ -89,7 +95,6 @@ public class Life {
                 }
 
             }
-            System.out.println();
 
         }
     }
@@ -110,7 +115,7 @@ public class Life {
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j].equals("O"))
+                if (grid[i][j].equals("X"))
                     return false;
             }
         }
