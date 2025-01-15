4d341fe6d0c918109be18bad9b75f7c6f4111c0b
1234persons <gibreeno@outlook.com>
2025-01-15 08:21:16 -0500

    modified:   src/Midterm/DungeonsClient.java

--- src/Midterm/DungeonsClient.java
+++ src/Midterm/DungeonsClient.java
@@ -7,9 +7,11 @@ import java.util.Random;
 
 @SuppressWarnings("unused")
 public class DungeonsClient {
-
+    public int ropeX, ropeY, arrowX, arrowY, dragonX, dragonY, playerX, playerY, ropeNum, arrowNum;
+    
+    Random rand = new Random();
     // MAIN PANEL
-    JPanel whatamidoingwithmylife = new JPanel();
+    JPanel mainPane = new JPanel();
     // Grid components
     JPanel gridPane = new JPanel();
     JLabel[][] dungeonTiles = new JLabel[10][10];
@@ -18,19 +20,34 @@ public class DungeonsClient {
 
     // Userside components
     JPanel userPane = new JPanel();
-    JLabel arrowLabel, ropeLabel, alertLabel = new JLabel();
+    JLabel arrowLabel, ropeLabel, alertLabel;
 
     public DungeonsClient() {
-        Random rand = new Random();
+        
         /* Create and set up the frame */
         frame = new JFrame("Dungeons and Dragons");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setForeground(Color.black);
 
         gridPane.setLayout(new GridLayout(10, 10, 0, 0));
         userPane.setLayout(new BoxLayout(userPane, BoxLayout.PAGE_AXIS));
+        mainPane.setLayout(new BoxLayout(mainPane, BoxLayout.PAGE_AXIS));
+        userPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        
 
-        int tempXcord = rand.nextInt(10);
-        int tempYcord = rand.nextInt(10);
+        //userPane Contents
+        arrowLabel = new JLabel(new ImageIcon(new ImageIcon("i:\\Downloads\\pixil-frame-0 (5).png").getImage().getScaledInstance(67, 67, 0)));
+        arrowLabel.setAlignmentX(JLabel.LEFT_ALIGNMENT);
+        arrowLabel.setBackground(Color.BLACK);
+        userPane.add(arrowLabel);
+
+        //userPane Contents
+        ropeLabel = new JLabel(new ImageIcon(new ImageIcon("i:\\Downloads\\pixil-frame-0 (6).png").getImage().getScaledInstance(67, 67, 0)));
+        ropeLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        ropeLabel.setBackground(Color.BLACK);
+        userPane.add(ropeLabel);
+
+        //gridPane contents
 
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 10; y++) {
@@ -38,27 +55,11 @@ public class DungeonsClient {
                 dungeonTiles[x][y] = new JLabel(tiles[x][y].getIcon());
             }
         }
-        tiles[tempXcord][tempYcord].setItem("rope");
-        dungeonTiles[tempXcord][tempYcord].setIcon(tiles[tempXcord][tempYcord].getIcon());
 
-        tempXcord = rand.nextInt(10);
-        tempYcord = rand.nextInt(10);
-        tiles[tempXcord][tempYcord].setItem("arrow");
-        dungeonTiles[tempXcord][tempYcord].setIcon(tiles[tempXcord][tempYcord].getIcon());
-
-        tempXcord = rand.nextInt(10);
-        tempYcord = rand.nextInt(10);
-        tiles[tempXcord][tempYcord].setItem("dragon");
-
-
-        for (int pits = 0; pits < 10; pits++) {
-            tempXcord = rand.nextInt(10);
-            tempYcord = rand.nextInt(10);
-            if (!tiles[tempXcord][tempYcord].getItem().equals("rope")) {
-                tiles[tempXcord][tempYcord].setItem("pit");
-                dungeonTiles[tempXcord][tempYcord].setIcon(tiles[tempXcord][tempYcord].getIcon());
-            }
-        }
+        placeArrow();
+        placeRope();
+        placeDragon();
+        placePits();
 
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 10; y++) {
@@ -66,13 +67,57 @@ public class DungeonsClient {
             }
         }
 
-        // if ()
+        if (playerX == arrowX && playerY == arrowY) {
+            JOptionPane itemPickup = new JOptionPane("You have found the arrow!");
+            itemPickup.setBackground(Color.green);
+            JOptionPane.showMessageDialog(frame, "You have found the arrow!");
+            arrowNum++;
+
+       }
+
+       gridPane.setAlignmentY(JPanel.TOP_ALIGNMENT);
+       userPane.setAlignmentY(JPanel.BOTTOM_ALIGNMENT);
+       mainPane.add(gridPane); 
+       mainPane.add(userPane); 
+
         /* Add content pane to frame */
-        frame.setContentPane(gridPane);
+        frame.setContentPane(mainPane);
 
         /* Size and then display the frame. */
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void placeArrow() {
+        arrowX = rand.nextInt(10);
+        arrowY = rand.nextInt(10);
+        tiles[arrowX][arrowY].setItem("arrow");
+        dungeonTiles[arrowX][arrowY].setIcon(tiles[arrowX][arrowY].getIcon());
+    }
+
+    public void placeRope() {
+        ropeX = rand.nextInt(10);
+        ropeY = rand.nextInt(10);
+        tiles[ropeX][ropeY].setItem("rope");
+        dungeonTiles[ropeX][ropeY].setIcon(tiles[ropeX][ropeY].getIcon());
+    }
+
+    public void placeDragon() {
+        dragonX = rand.nextInt(10);
+        dragonY = rand.nextInt(10);
+        tiles[dragonX][dragonY].setItem("dragon");
+        dungeonTiles[dragonX][dragonY].setIcon(tiles[dragonX][dragonY].getIcon());
+    }
+
+    public void placePits() {
+        for (int pits = 0; pits < 10; pits++) {
+            int tempXcord = rand.nextInt(10);
+            int tempYcord = rand.nextInt(10);
+            if (!tiles[tempXcord][tempYcord].getItem().equals("rope") && !tiles[tempXcord][tempYcord].getItem().equals("rope")) {
+                tiles[tempXcord][tempYcord].setItem("pit");
+                dungeonTiles[tempXcord][tempYcord].setIcon(tiles[tempXcord][tempYcord].getIcon());
+            }
+        }
     }
 
     /**
