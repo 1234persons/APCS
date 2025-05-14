629bf0a645e4035b535d7c067fd0439bc57da512
1234persons <150205031+1234persons@users.noreply.github.com>
2025-05-14 05:45:49 -0400

    Update PolygonObject.java

--- src/Final/PolygonObject.java
+++ src/Final/PolygonObject.java
@@ -3,29 +3,42 @@ package Final;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Polygon;
-import javax.swing.*;
+
 
 public class PolygonObject {
 
     Polygon p;
     Color c;
+    double avgsdist = 0;
+    boolean draw = true;
     public PolygonObject(double[] x, double[] y, Color c) {
-
         p = new Polygon();
-
+        
         for (int i = 0; i < x.length; i++) {
-
             p.addPoint((int) x[i], (int) y[i]);
-
         }
-
         this.c = c;
+    }
+
+    void update2gon(double[] x, double[] y) {
+        p.reset();
+        for (int i = 0; i < x.length; i++) {
+            p.xpoints[i] = (int) x[i];
+            p.ypoints[i] = (int) y[i];
+            p.npoints = (int) x.length;
+        }
 
     }
 
     void drawPolygon(Graphics g) {
-        g.setColor(c);
-        g.drawPolygon(p);
+        if (draw) {
+            g.setColor(c);
+            g.fillPolygon(p);
+            g.setColor(Color.BLACK);
+            g.drawPolygon(p); 
+        }
+        
+
     }
 
     
